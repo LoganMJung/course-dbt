4eb07a5837ca87d9d96cdf93790b60efea99c0aa
Logan Jung <LoganMathewJung@gmail.com>
2024-04-09 01:07:50 +0000

    your commit message

--- greenery/models/marts/product/fact_page_views.sql
+++ greenery/models/marts/product/fact_page_views.sql
@@ -1,10 +1,33 @@
-SELECT
-    event_guid,
-    created_at_utc,
-    session_guid,
-    user_guid,
-    page_url
-FROM
-    {{ ref('stg_postgres__events') }}
-WHERE
-    event_type = 'page_view'
+with
+events as (
+    select * from {{ ref('stg_postgres__events') }}
+),
+
+order_items as (
+    select * from {{ ref('stg_postgres__order_items') }}
+),
+
+session_timing_agg as (
+    select
+        session_guid,
+        min(created_at_utc) as session_started_at_utc,
+        max(created_at_utc) as session_ended_at_utc
+    from {{ ref('stg_postgres__events') }}
+    group by 1
+)
+
+select
+    e.session_guid,
+    e.user_guid,
+    coalesce(e.product_guid, oi.product_guid) as product_guid,
+    session_started_at_utc,
+    session_ended_at_utc,
+    sum(case when e.event_type = 'page_view' then 1 else 0 end) as page_views,
+    sum(case when e.event_type = 'add_to_cart' then 1 else 0 end) as add_to_carts,
+    sum(case when e.event_type = 'checkout' then 1 else 0 end) as checkouts,
+    sum(case when e.event_type = 'package_shipped' then 1 else 0 end) as packages_shipped,
+    datediff('minute', session_started_at_utc, session_ended_at_utc) as session_length_minutes
+from events e
+left join order_items oi on oi.order_guid = e.order_guid
+left join session_timing_agg s on s.session_guid = e.session_guid
+group by 1, 2, 3, 4, 5
